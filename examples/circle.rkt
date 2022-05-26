01d531e2edcd044b3c4e2b2138735af452cc7853
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-05-26 14:10:25 +0000

    Create circle.rkt

---
+++ examples/circle.rkt
@@ -0,0 +1,84 @@
+#lang racket
+
+(require sfml)
+
+#|
+this program is a clone of ex_3.rkt from
+the racket-csfml examples
+|#
+
+; define your objects like so
+(define window (new render-window%))
+(define circle (new circle-shape%))
+
+; make the main window
+(send* window
+  (make (video-mode 800 600 32) "SFML Works! ^,..,^" '(sfDefaultStyle))
+  (set-visible #f))
+
+; make the circle
+(send* circle
+  (make)
+  (set-radius 20)
+  (set-fill-color (color 0 0 0 0))
+  (set-outline-color (color 255 0 255))
+  (set-outline-thickness 3)
+  (set-origin (vector2f 20 20)))
+
+; convert integer pair to vector2 in view space
+(define (map-pixel x y)
+  (let ([pos (vector2i x y)]
+        [view (send window get-view)])
+    (send window map-pixel-to-coords pos view)))
+
+; poll your window's events
+(define (poll-events)
+  (let ([e (send window poll-event)])
+    (when e
+      (case (event-type e)
+        ('sfEvtClosed
+         (send window close))
+        ('sfEvtMouseMoved
+         (let* ([pos (map-pixel (event-mouse-move-x e)
+                                (event-mouse-move-y e))])
+                (send circle set-position pos)))
+        ('sfEvtKeyPressed
+         (case (event-key-code e)
+           ('sfKeyEscape
+            (send window close)))))
+      (poll-events))))
+
+; render your circle
+(define (render)
+  (send* window
+    (clear)
+    (draw circle)
+    (display)))
+
+(define (game-loop)
+  (poll-events)
+  (render)
+  (when (send window is-open?)
+    (game-loop)))
+
+(send* window
+  (set-visible #t)
+  (clear)
+  (display))
+
+(with-handlers
+    ([exn?
+     (λ (exn)
+       (displayln exn)
+       (send window close))])
+     (game-loop))
+
+#|
+always kill your sfml objects when finished with them to free the memory
+they are still csfml pointers under the hood, after all
+I like to define an object manager class with a kill-all function
+to do this in one command and keep my code cleaner, usually
+|#
+
+(send window kill)
+(send circle kill)
