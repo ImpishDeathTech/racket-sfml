03174f01a6e865f9c30ed177a34b58a0c3655bb0
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-05-31 16:44:52 +0000

    Update version

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "sfml")
 (define pkg-authors '("ImpishDeathTech@protonmail.ch"))
-(define version "0.6")
+(define version "0.7")
 (define pkg-desc "Object Oriented, kebab cased layer for racket-csfml")
 (define deps '("base" "csfml"))
 (define compile-omit-paths '("examples"))
